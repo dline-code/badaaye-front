6e44a85dc517b242755c8cd0ed205af36f471ea8
JulianaSobaJava <julianasobajava23@gmail.com>
2022-09-01 00:33:58 +0100

    feat: adiciona transição ao clicar na pergunta do accordion

--- src/components/frequent-question/index.tsx
+++ src/components/frequent-question/index.tsx
@@ -8,11 +8,14 @@ import 'aos/dist/aos.css'
 
 const frequentQuestions: React.FC = () => {
   const [clicked, setCliked] = useState<number>();
-  const identifier = useRef<HTMLLIElement>(null);
+  const identifier = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     Aos.init({ duration: 2000 })
   })
+
+  let panel = identifier.current?.nextElementSibling;
+  console.log(panel?.scrollHeight)
 
   return (
     <S.Container>
@@ -21,8 +24,8 @@ const frequentQuestions: React.FC = () => {
       <S.AccordionList data-aos="zoom-in">
         {questions.map((value: FrequentQuestionsProps)=>(
           <S.Item key={value.id}>
-            <button onClick={() => clicked !== undefined && clicked === value.id ? setCliked(undefined) : setCliked(value.id)}><span>{value.id}. {value.question}</span>  <span className="fa-plus">{clicked === value.id ? <FiMinus /> : <FiPlus />}</span></button>
-            <div className={clicked === value.id ? 'open' : 'close'}>
+            <button ref={identifier} onClick={() => clicked !== undefined && clicked === value.id ? (setCliked(undefined) ): setCliked(value.id)}><span>{value.id}. {value.question}</span>  <span className="fa-plus">{clicked === value.id ? <FiMinus /> : <FiPlus />}</span></button>
+            <div className={clicked === value.id ? 'open' : 'close'} style={clicked === value.id ?{ maxHeight: panel?.scrollHeight + 'px' }: { maxHeight: 0}}>
               <p>{value.answer}</p>
             </div>
           </S.Item>
