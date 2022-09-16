41a2bfdbc0c828d349c49e6ae6445ec85cc95ba6
José Gonçalves <goncalves2541@gmail.com>
2022-09-16 01:20:28 +0100

    remove example component

--- src/components/frequent-question/index.tsx
+++ src/components/frequent-question/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import * as S from './styles'
 import { FrequentQuestionsProps } from './type'
 import { questions } from './mock'
@@ -7,31 +7,53 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const frequentQuestions: React.FC = () => {
-  const [clicked, setCliked] = useState<number>();
-  const identifier = useRef<HTMLButtonElement>(null);
+  const [clicked, setCliked] = useState<number>()
+  const identifier = useRef<HTMLButtonElement>(null)
 
   useEffect(() => {
     Aos.init({ duration: 2000 })
   })
 
-  let panel = identifier.current?.nextElementSibling;
-  console.log(panel?.scrollHeight)
+  let panel = identifier.current?.nextElementSibling
 
   return (
     <S.Container>
       <S.Title>Perguntas frequentes</S.Title>
 
       <S.AccordionList data-aos="zoom-in">
-        {questions.map((value: FrequentQuestionsProps)=>(
+        {questions.map((value: FrequentQuestionsProps) => (
           <S.Item key={value.id}>
-            <button ref={identifier} onClick={() => clicked !== undefined && clicked === value.id ? (setCliked(undefined) ): setCliked(value.id)}><span>{value.id}. {value.question}</span>  <span className="fa-plus">{clicked === value.id ? <FiMinus /> : <FiPlus />}</span></button>
-            <div className={clicked === value.id ? 'open' : 'close'} style={clicked === value.id ?{ maxHeight: panel?.scrollHeight + 'px' }: { maxHeight: 0}}>
+            <button
+              ref={identifier}
+              onClick={() =>
+                clicked !== undefined && clicked === value.id
+                  ? setCliked(undefined)
+                  : setCliked(value.id)
+              }
+            >
+              <span>
+                {value.id}. {value.question}
+              </span>{' '}
+              <span className="fa-plus">
+                {clicked === value.id ? <FiMinus /> : <FiPlus />}
+              </span>
+            </button>
+            <div
+              className={clicked === value.id ? 'open' : 'close'}
+              style={
+                clicked === value.id
+                  ? { maxHeight: panel?.scrollHeight + 'px' }
+                  : { maxHeight: 0 }
+              }
+            >
               <p>{value.answer}</p>
             </div>
           </S.Item>
         ))}
       </S.AccordionList>
-      <S.ContactIndicate>Se tiver dúvidas entre em contacto connosco <FiArrowDown/> </S.ContactIndicate>
+      <S.ContactIndicate>
+        Se tiver dúvidas entre em contacto connosco <FiArrowDown />{' '}
+      </S.ContactIndicate>
     </S.Container>
   )
 }
