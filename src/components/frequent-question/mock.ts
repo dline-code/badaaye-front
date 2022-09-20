d2249e6d0676a6ce7addf6146f3f54b3895e6007
JulianaSobaJava <julianasobajava23@gmail.com>
2022-09-20 18:06:38 +0100

    aidciona alterações de texto nos componentes

--- src/components/frequent-question/mock.ts
+++ src/components/frequent-question/mock.ts
@@ -3,19 +3,19 @@ export const questions = [
     id: 1,
     question: 'Quem nós somos',
     answer:
-      'At Segment, we believe companies should be able to send their customer data wherever they want, whenever they want, with no fuss. We make this easy with a single pipeline that collects, store. able to send their customer data wherever they want, whenever they want, with no fuss. We make this easy with a single pipeline that collects, stores, filters. At Segment, we believe companies should be able to send their customer data wherever they want, whenever they want, with no fuss. We make this easy with a single pipeline that collects, stores, filters.'
+      'Nascemos do pensamento das dificuldades da vida académica de cada estudante brilhante. Criamos conexões com instituições comerciais, filantrópicas e padrinhos, fazedores de boa vontade, para ceder bolsas de estudo para os que precisam de apoio. Queremos diminuir a desigualdade societária e construir um país melhor apostando na formação de quadros qualificados, diminuindo as dificuldades de acesso às Universidades.'
   },
   {
     id: 2,
     question: 'Tipos de financiamentos',
     answer:
-      'At Segment, we believe companies should be able to send their customer data wherever they want, whenever they want, with no fuss. We make this easy with a single pipeline that collects, stores, filters.'
+      'Os financiamentos aqui citados são meramente bolsas de estudo de instituições comerciais, instituições filantrópicas e de padrinhos, descontos de propinas em parceria com as instituições de ensino, descontos de compras de artigos nas lojas parceiras, descontos no  pagamento de transportes coletivos.'
   },
   {
     id: 3,
     question: 'Como solicitar um financimento',
-    answer:
-      'Transforms, and sends data to hundreds of business tools with the flip of a switch. Historically,  built integrations with more than 250 different customer data tools ourselves(think Mixpanel).'
+    answer:['Existem dois principais financiamentos: Bolsa de estudos e descontos de propinas.','Para Bolsas de estudos,  primeiro deve inscrever-se na plataforma,  preencher o perfil com informações credíveis,  fazer um vídeo de apresentação apresentando os motivos que o levam a pedir bolsas de estudos, anexar a situação financeira em documento no perfil criado.','Para descontos de propinas: inscrever-se na plataforma, preencher o perfil, escolher a universidade parceira, escolher o curso e efectuar inscrição .'
+    ]
   },
   {
     id: 4,
