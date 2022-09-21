d0d9c40eb931c978eb5982918f83160f90ca15de
Margarida-Andre <margaridaandre034@gmail.com>
2022-09-21 16:00:27 +0100

    feat: actualiza logo das instituições UCAN e Piaget

--- src/components/contactos/index.tsx
+++ src/components/contactos/index.tsx
@@ -1,51 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as S from './styles'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsTelephone } from 'react-icons/bs'
 import { TiLocationOutline } from 'react-icons/ti'
-import Map from '../map-dline/index'
+import Map from '../map-baadaye/index'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
-const Contactos = () => (
-  <S.ContainerContactos>
-    <S.ContainerObject>
-      <S.Container1 data-aos="fade-right">
-        <h1>Em caso de dúvida entre em contacto</h1>
-        <S.ContainerCampo>
-          <span>Nome Completo</span>
-          <input type="text" />
-        </S.ContainerCampo>
-        <S.ContainerCampo>
-          <span>Sem e-mail</span>
-          <input type="text" />
-        </S.ContainerCampo>
-        <S.ContainerCampo>
-          <span>Mensagem</span>
-          <input type="text" className="sms" />
-        </S.ContainerCampo>
-        <S.Button>Enviar Mensagem</S.Button>
-      </S.Container1>
-      <S.Container2 data-aos="fade-left">
-        <div>
-          <TiLocationOutline className="icon" />
-          <span>Luanda, Rua xxx, yyy, zzz</span>
-        </div>
+const Contactos = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  })
+  return (
+    <S.ContainerContactos>
+      <S.ContainerObject>
+        <S.Container1 data-aos="fade-right">
+          <h1>Em caso de dúvida entre em contacto</h1>
+          <S.ContainerCampo>
+            <span>Nome Completo</span>
+            <input type="text" />
+          </S.ContainerCampo>
+          <S.ContainerCampo>
+            <span>Sem e-mail</span>
+            <input type="text" />
+          </S.ContainerCampo>
+          <S.ContainerCampo>
+            <span>Mensagem</span>
+            <input type="text" className="sms" />
+          </S.ContainerCampo>
+          <S.Button>Enviar Mensagem</S.Button>
+        </S.Container1>
+        <S.Container2 data-aos="fade-left">
+          <div>
+            <TiLocationOutline className="icon" />
+            <span>Luanda, Rua xxx, yyy, zzz</span>
+          </div>
 
-        <div>
-          <AiOutlineMail className="icon" />
-          <span>baadaye@dominio.com</span>
-        </div>
+          <div>
+            <AiOutlineMail className="icon" />
+            <span>baadaye@dominio.com</span>
+          </div>
 
-        <div>
-          <BsTelephone className="icon" />
-          <span>(+244) 920 000 000</span>
-        </div>
-        <Map />
-      </S.Container2>
-    </S.ContainerObject>
-  </S.ContainerContactos>
-)
+          <div>
+            <BsTelephone className="icon" />
+            <span>(+244) 920 000 000</span>
+          </div>
+          <Map />
+        </S.Container2>
+      </S.ContainerObject>
+    </S.ContainerContactos>
+  )
+}
 
 export default Contactos
 /*import * as S from './styles'
