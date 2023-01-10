0b20dcd4124cc6646f6b3441bef903bdf8e7e6aa
menezescosta <mc611641@gmail.com>
2023-01-10 19:02:15 +0100

    fix: adiciona responsividade no cadastro de estudante

--- src/views/cadastro-estudante/styles.ts
+++ src/views/cadastro-estudante/styles.ts
@@ -3,17 +3,26 @@ import styled from 'styled-components'
 export const Wrapper = styled.main`
   width: 100%auto;
   height: 100vh;
+  padding: 0 10rem;
+  background: ${({theme})=> theme.colors.tertiary.white};
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 0 10rem;
-`
 
+  @media screen and (max-width: 1280px){
+    padding: ${({theme})=> theme.spacings.xmultipleeight};
+  }
+  @media screen and (max-width: 920px){
+    height: 100%;
+    padding: ${({theme})=> theme.spacings.xxmultipleeight} 0;
+    flex-direction: column-reverse;
+  }
+`
 export const Form = styled.form`
   width: 32%;
-  height: 55rem;
-  padding: 5rem 4rem;
+  height: max-height;
+  padding: ${({theme})=> theme.spacings.xmultipleeight} 4rem;
   background: #f0f0f0;
   border-radius: .8rem;
 
@@ -22,7 +31,21 @@ export const Form = styled.form`
   }
 
   .button {
-    margin-top: 2.5rem;
+    margin-top: ${({theme})=> theme.spacings.xxmultipleeight};
+  }
+
+  @media screen and (max-width: 1280px){
+    width: 35%;
+  }
+  @media screen and (max-width: 1000px){
+    width: 50%;
+  }
+  @media screen and (max-width: 920px){
+    margin: 0 0 !important;
+    width: 80%;
+  }
+  @media screen and (max-width: 550px){
+    width: 90%;
   }
 `
 
@@ -53,8 +76,26 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   margin-left: 12rem;
-`
 
+  @media screen and (max-width: 1280px){
+    width: 36%;
+
+    label {
+      height: 12rem;
+      margin-bottom: ${({theme})=> theme.spacings.xxxxmultipleeight};
+    }
+  }
+  @mediascreen and(max-width: 1000px){
+    width: 50%;
+  }
+  @media screen and (max-width: 920px){
+    width: 80%;
+    margin-left: 0 !important;
+  }
+  @media screen and (max-width: 550px){
+    width: 90%;
+  }
+`
 export const ContainerRadios = styled.div`
   width: 100%auto;
   height: 23.5rem;
@@ -62,13 +103,17 @@ export const ContainerRadios = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  @media screen and (max-width: 920px){
+    margin-bottom: 3.2rem;
+  }
 `
 
 export const Text = styled.div`
   font-size: ${({ theme }) => theme.font.sizes.midle};
   color: ${({ theme }) => theme.colors.secondary.xxxblue};
   font-weight: ${({ theme }) => theme.font.smallbold};
-  margin-top: 3rem;
+  margin: 3rem 0;
   display: flex;
   flex-direction: row;
   align-items: center;
