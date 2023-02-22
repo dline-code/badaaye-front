88080bd103e6f558af50d71616e14c754ebf452c
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-02-22 11:18:12 +0100

    refact: refatorando o codigo

---
+++ src/views/cadastro-estudante/hooks/useStudent.tsx
@@ -0,0 +1,49 @@
+import { useFormik } from "formik"
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { postFecthStudent1 } from "../services";
+import { IErrorInterface, IStudent } from "../type";
+
+
+const UseStudent = () => {    
+    async function Studentregistration1(data: IStudent){
+        try{
+            const student = await postFecthStudent1(data)
+            if(student) toast("Cadastro feito com sucesso", {autoClose: 2000, type: "success"})
+    
+        } catch(err){
+            const error = err as IErrorInterface
+            toast(error.response?.data?.error, {autoClose: 2000, type: "error"})
+        }   
+    }
+    
+    const formik = useFormik({
+        initialValues:{
+          nome: "",
+          sobrenome: "",
+          dataNascimento: "",
+          email: "",
+          telefone: "",
+          senha: "",
+          confirmarSenha: ""
+        },
+        validationSchema: yup.object({
+          nome: yup.string().required("O nome é obrigatorio"),
+          sobrenome: yup.string().required("O Sobrenome é obrigatorio"),
+          telefone: yup.string().required("O número de telefone é obrigatorio"),
+          dataNascimento: yup.string().required("A data de Nascimento é obrigatoria"),
+          email: yup.string().email("digite um email valido").required("O email é obrigatorio"),
+          senha: yup.string().min(8, "a senha deve ter pelo menos 8 digitos").required("a senha é obrigatoria"),
+          confirmarSenha: yup.string().required("A confirmação de senha é obrigatoria").oneOf([yup.ref("senha")], "As senhas não correspondem"),
+        }),
+        onSubmit: (data) => Studentregistration1(data)
+    })
+    
+    return{
+        formik,
+    }
+
+}
+
+
+export { UseStudent }
--- src/views/cadastro-estudante/index.tsx
+++ src/views/cadastro-estudante/index.tsx
@@ -11,48 +11,13 @@ import { BsFillTelephoneFill } from 'react-icons/bs'
 import Link from 'next/link'
 import Input from 'src/components/input'
 import Button from 'src/components/button'
-import * as yup from "yup";
-import { useFormik } from 'formik'
-import { IErrorInterface, IStudent } from './type'
-import { postFecthStudent1 } from './services'
-import { toast } from "react-toastify"
+import { UseStudent } from './hooks/useStudent'
+
 
 const cadastroEstudante: NextPage = () => {
-
-  async function Validation(data: IStudent){
-    try{
-      const student = await postFecthStudent1(data)
-      if(student) toast("Cadastro feito com sucesso", {autoClose: 2000, type: "success"})
-
-    } catch(err){
-      const error = err as IErrorInterface
-      toast(error.response?.data?.error, {autoClose: 2000, type: "error"})
-    }   
-  }
   
-  const formik = useFormik({
-    initialValues:{
-      nome: "",
-      sobrenome: "",
-      dataNascimento: "",
-      email: "",
-      telefone: "",
-      senha: "",
-      confirmarSenha: ""
-    },
-    validationSchema: yup.object({
-      nome: yup.string().required("O nome é obrigatorio"),
-      sobrenome: yup.string().required("O Sobrenome é obrigatorio"),
-      telefone: yup.string().required("O número de telefone é obrigatorio"),
-      dataNascimento: yup.string().required("A data de Nascimento é obrigatoria"),
-      email: yup.string().email("digite um email valido").required("O email é obrigatorio"),
-      senha: yup.string().min(8, "a senha deve ter pelo menos 8 digitos").required("a senha é obrigatoria"),
-      confirmarSenha: yup.string().required("A confirmação de senha é obrigatoria").oneOf([yup.ref("senha")], "As senhas não correspondem"),
-    }),
-    onSubmit: (data) => Validation(data)
-  })
+  const { formik } = UseStudent()
   
-    
 
   return (
     <S.Wrapper>
