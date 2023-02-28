5c97a88975aee1d37bf552a5479b7ee33851da5a
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-02-28 16:24:39 +0100

    feat: iniciando a criacao da tela principal do estudante

---
+++ src/pages/tela-principal-estudante/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next"
+
+
+const TelaPrincipalEstudante: NextPage = () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export { TelaPrincipalEstudante }
