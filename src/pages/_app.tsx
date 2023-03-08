cd7dce99c086741cb56f7529ae2a3a43b43e3084
menezescosta <mc611641@gmail.com>
2023-03-08 11:42:11 +0100

    feat: adicionando queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,7 @@ import { Layout } from '../components/layout'
 import 'aos/dist/aos.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query';
+import {QueryClient,QueryClientProvider,} from 'react-query';
 
 const queryClient = new QueryClient();
 
