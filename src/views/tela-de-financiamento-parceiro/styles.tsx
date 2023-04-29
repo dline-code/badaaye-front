8456900f1b8a24304e3cc308e1a094020311372a
Osvaldo943 <osvaldodesousa9432@gmail.com>
2023-04-29 20:35:32 +0100

    perf: improve tela-de-financiamento page

--- src/views/tela-de-financiamento-parceiro/styles.tsx
+++ src/views/tela-de-financiamento-parceiro/styles.tsx
@@ -1,5 +1,91 @@
 import styled from "styled-components"
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
     width: 100%;    
 `
+export const Container = styled.div`
+    width: 100%;
+    padding: 0 15rem;    
+`
+export const Content = styled.div`
+    width: 100%;
+    margin-top: -10rem;
+    padding: ${({theme})=> theme.spacings.xxxmultipleeight} 4rem;    
+    background: ${({ theme }) => theme.colors.tertiary.xxwhite};
+    border-radius: 4px;
+`
+export const Form = styled.div`
+    width: 100%;
+    display: flex;
+    height: 8rem;
+    border-bottom: 0.2rem solid ${({ theme }) => theme.colors.tertiary.gray};
+    margin-bottom: ${({theme})=> theme.spacings.xxxmultipleeight};
+
+    .button {
+        width: 20rem;
+        margin-left: ${({theme})=> theme.spacings.xxxmultipleeight};
+    }
+`
+
+export const FinancyTitles = styled.div`
+    width: 89%;
+    padding: ${({theme})=>theme.spacings.xxxxmultipleeight};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: ${({theme})=> theme.spacings.xxxmultipleeight};
+
+    div {
+        width: 25%;
+        font-size: ${({ theme }) => theme.font.sizes.midle};
+        font-weight: ${({ theme }) => theme.font.regular};
+    }
+`
+export const FinancyList = styled.ul`
+    width: 100%;
+`
+export const FinancyItem = styled.li`
+    width: 100%;
+    display: flex;
+    margin-bottom: ${({theme})=>theme.spacings.xxxxmultipleeight};
+    justify-content: space-between;
+    align-items: center;
+`
+export const LeftSide = styled.div`
+    width: 100%;
+    padding: ${({theme})=>theme.spacings.xxxxmultipleeight};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 0.1rem solid ${({ theme }) => theme.colors.tertiary.xcinza};
+    border-radius: 4px;
+   
+    div {
+        width: 25%;
+        font-size: ${({ theme }) => theme.font.sizes.midle};
+        font-weight: ${({ theme }) => theme.font.regular};
+    }
+`
+
+    export const RightSide = styled.div`
+        width: 10rem;
+        margin-left: ${({theme})=>theme.spacings.xxxxmultipleeight};
+
+        display: flex;
+        justify-content: space-between;
+
+        .button{
+            width: 4rem;
+            height: 4rem;
+
+            display: flex;
+            justify-content: center;      
+            align-items: center;
+
+            font-size: ${({theme})=> theme.font.sizes.xxlarge};
+
+            background: ${({ theme }) => theme.colors.primary.xblue};  
+            color: ${({ theme }) => theme.colors.tertiary.white};
+            border-radius: 4px;
+        }
+    `
