73331f3518c1ac533bd06e16ddf080bc1ebe67ec
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-04-29 08:53:59 +0100

    feat: create headercontent page

--- src/views/tela-estudantes-finaciados/index.tsx
+++ src/views/tela-estudantes-finaciados/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { Layout } from 'src/components/layout'
 import * as S from './styles'
 import { PageProps } from './types'
@@ -5,7 +7,25 @@ import { PageProps } from './types'
 export default function TelaEstudantesFinaciadosView(props: PageProps) {
   return (
     <Layout {...Object.assign({}, props, { hideFooter: true, isLogged: true })}>
-      dhjk
+      <S.Main>
+        <S.Content>
+          <S.ContentHeader>
+            <Link href={'../'}>
+              <a>
+                <AiOutlineArrowLeft />
+              </a>
+            </Link>
+
+            <h2> Estudantes financiados por si</h2>
+
+            <p>
+              Visualize os dados dos estudantes que estão a ser <br />
+              financiados ou já foram financiados por si
+            </p>
+          </S.ContentHeader>
+          <S.ContentBody></S.ContentBody>
+        </S.Content>
+      </S.Main>
     </Layout>
   )
 }
