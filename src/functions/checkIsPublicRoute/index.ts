3d34574b7b6d8e70623ac7a58d75ba5d72ef13a1
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-23 19:49:45 +0100

    chore: remove console log

--- src/functions/checkIsPublicRoute/index.ts
+++ src/functions/checkIsPublicRoute/index.ts
@@ -3,7 +3,5 @@ import { APP_ROUTES } from 'src/constants/app-routes'
 export const checkIsPublicRoute = (asPath: string) => {
   const appPublicRoutes = Object.values(APP_ROUTES.public)
 
-  console.log(asPath)
-
   return appPublicRoutes.includes(asPath)
 }
