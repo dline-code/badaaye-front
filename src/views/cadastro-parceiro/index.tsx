a5ee4e899668a5ea886f0a7f7ea1c0114ec0fe1a
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-06-28 21:16:39 +0100

    feat: resolvendo os conflitos

--- src/views/cadastro-parceiro/index.tsx
+++ src/views/cadastro-parceiro/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import type { NextPage } from 'next'
 import Link from 'next/link'
 
@@ -9,54 +7,98 @@ import LayoutAuthentication from '../../components/layout-authentication'
 import Input from 'src/components/input'
 import Button from 'src/components/button'
 import Select from 'src/components/select'
-import RadioBox from 'src/components/radiobox'
 
 import { MdEmail } from 'react-icons/md'
 import { HiUser, HiArrowNarrowLeft } from 'react-icons/hi'
 import { RiLockPasswordFill } from 'react-icons/ri'
-import { BsBriefcaseFill, BsFillTelephoneFill, BsPhoneFill } from 'react-icons/bs'
+import { BsBriefcaseFill, BsPhoneFill } from 'react-icons/bs'
+import { usePartner } from './hooks/usePartner'
+import { useFetch } from 'src/hooks/useFetch'
+import { ToggleRegisterType } from 'src/components/ToggleRegisterType'
 
 const Parceiro: NextPage = () => {
+  const { data: partnerType, isLoading } = useFetch(`/tipoParceiro`)
+  const { formik, isSubmitting } = usePartner()
+
+  if (isLoading) return <>Carregando...</>
+
   return (
     <S.Wrapper>
-      <S.Form>
+      <S.Form onSubmit={formik.handleSubmit}>
         <S.Title>Crie sua conta</S.Title>
         <Input
           type="text"
           placeholder="Nome da entidade"
+          name="nome"
+          onChange={formik.handleChange}
           icon={<HiUser />}
         />
+        {formik.touched.nome && formik.errors.nome ? (
+          <S.ErrorMessage>{formik.errors.nome}</S.ErrorMessage>
+        ) : null}
+
         <Input
           type="email"
           placeholder="Seu e-mail"
+          name="email"
+          onChange={formik.handleChange}
           icon={<MdEmail />}
         />
+        {formik.touched.email && formik.errors.email ? (
+          <S.ErrorMessage>{formik.errors.email}</S.ErrorMessage>
+        ) : null}
+
         <S.SelectContainer>
           <Select
             title="parceiro"
-            options={[{nome:"parceiro1",id:"1"}, {nome:"Parceiro2",id:"2"}]}
+            options={partnerType}
+            name="tipoParceiroId"
+            onChange={formik.handleChange}
             icon={<BsBriefcaseFill />}
           />
+          {formik.touched.tipoParceiroId && formik.errors.tipoParceiroId ? (
+            <S.ErrorMessage>{formik.errors.tipoParceiroId}</S.ErrorMessage>
+          ) : null}
         </S.SelectContainer>
+
         <Input
           type="number"
           placeholder="Número de telefone"
+          name="telefone"
+          onChange={formik.handleChange}
           icon={<BsPhoneFill />}
         />
+        {formik.touched.telefone && formik.errors.telefone ? (
+          <S.ErrorMessage>{formik.errors.telefone}</S.ErrorMessage>
+        ) : null}
+
         <Input
           type="password"
           placeholder="Sua senha"
-          icon={<RiLockPasswordFill />
-          }
-        />
-        <Input type="password"
-          placeholder="Comfirme sua senha"
+          name="senha"
+          onChange={formik.handleChange}
           icon={<RiLockPasswordFill />}
         />
-        <Button>Cadastrar</Button>
+        {formik.touched.senha && formik.errors.senha ? (
+          <S.ErrorMessage>{formik.errors.senha}</S.ErrorMessage>
+        ) : null}
+
+        <Input
+          type="password"
+          placeholder="Comfirme sua senha"
+          name="confirmeSenha"
+          onChange={formik.handleChange}
+          icon={<RiLockPasswordFill />}
+        />
+        {formik.touched.confirmeSenha && formik.errors.confirmeSenha ? (
+          <S.ErrorMessage>{formik.errors.confirmeSenha}</S.ErrorMessage>
+        ) : null}
+
+        <Button disabled={isSubmitting}>Cadastrar</Button>
 
         <S.Description>
-          Ao se cadastrar, você aceita todos nossos <Link href="/">
+          Ao se cadastrar, você aceita todos nossos{' '}
+          <Link href="/">
             <S.Ancora>política de privacidade</S.Ancora>
           </Link>
         </S.Description>
@@ -72,25 +114,8 @@ const Parceiro: NextPage = () => {
             <S.LinkDescription>Voltar para login</S.LinkDescription>
           </S.TextBack>
         </Link>
-        <S.ContainerRadios>
-          <Link href="/cadastro-estudante" >
-            <RadioBox
-              title="Estudante"
-              description="Aluno com o ensino médio concluído frequentando ou não o ensino superior."
-              src="/assets/library-bolsas.svg"
-              isActive={false}
-            />
-          </Link>
-          <Link href="#">
-            <RadioBox
-              title="Parceiro"
-              description="Entidades dispostas a contribuir para
-              sucesso acádemico dos estudantes."
-              src="/assets/parceiro.svg"
-              isActive={true}
-            />
-          </Link>
-        </S.ContainerRadios>
+
+        <ToggleRegisterType checkedOne="parceiro" />
 
         <Link href="/login">
           <S.Text>
