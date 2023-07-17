d6c1e2e5125a72e89ab86cf8579dfd30d2cae5d0
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-07-17 20:00:26 +0100

    fix: authorization to get financing Data

--- src/views/tela-estudantes-finaciados/hooks/index.ts
+++ src/views/tela-estudantes-finaciados/hooks/index.ts
@@ -2,19 +2,19 @@ import { useQuery } from 'react-query'
 import { api } from 'src/services/api'
 import { FinancingType } from '../types'
 import { useAuth } from 'src/hooks/useAuth'
+import { getCookie } from 'cookies-next'
 
 export const useFetchData = () => {
   const { usuarioId } = useAuth()
+  const token = getCookie(`${process.env.NEXT_PUBLIC_TOKEN_KEY}`)?.toString()
 
-  const {
-    data: financingData,
-    isFetched,
-    isFetching,
-    isLoading
-  } = useQuery<FinancingType[]>('financingData', async () => {
-    const response = await api.get(`/financiamento/parceiro/${usuarioId}`)
+  return useQuery<FinancingType[]>('financingData', async () => {
+    const response = await api.get(`/financiamento/parceiro/${usuarioId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
 
     return response.data
   })
-  return { financingData, isFetched, isFetching, isLoading }
 }
